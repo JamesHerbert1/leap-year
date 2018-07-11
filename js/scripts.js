18c2d9bebaafc8c313c52df1835edff264f9f870
JamesHerbert1 <agembejames1997@gmail.com>
2018-07-11 11:13:58 +0300

    added a function to my js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// business logic
+var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// user interface logic
 $(document).ready(function() {
     $("form#leap-year").submit(function(event) {
         event.preventDefault();
@@ -6,15 +16,34 @@ $(document).ready(function() {
         
         var intYear = parseInt(year)
         var result = leapYear(intYear);
-        $("#result").text(result);
+        $(".year").text(year);
+        
+        if (!result) {                 // same as writing if (result === false)
+            $(".not").text("not");
+          } else {
+            $(".not").text("");
+          }
+    
+          $("#result").show();
 
     });
 });
 
-var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-        return true;
-    } else {
-        return false;
-    }
-};
+// $(document).ready(function() {
+//     $("form#leap-year").submit(function(event) {
+//       event.preventDefault();
+//       var year = parseInt($("input#year").val());
+//       var result = leapYear(year);
+
+//       $(".year").text(year);
+
+//       if (!result) {                 // same as writing if (result === false)
+//         $(".not").text("not");
+//       } else {
+//         $(".not").text("");
+//       }
+
+//       $("#result").show();
+//     });
+//   });
+  
